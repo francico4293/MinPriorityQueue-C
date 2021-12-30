68341b2a2c9e59ba1f0bc0a0299218be56b97b31
francico4293 <francico@oregonstate.edu>
2021-12-29 20:07:02 -0500

    Initial commit

---
+++ main.c
@@ -0,0 +1,11 @@
+/**
+ * @file main.c
+ * @author Colin Francis
+ * @brief Minimum priority queue implementation using an array based
+ * heap in C
+ * @version 0.1
+ * @date 2021-12-29
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
