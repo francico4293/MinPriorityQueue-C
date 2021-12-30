840ec031e42d8c965e212738c50683b58f5cd2c7
francico4293 <francico@oregonstate.edu>
2021-12-29 20:15:00 -0500

    Created a function to create a new dynamic array struct

--- main.c
+++ main.c
@@ -9,3 +9,34 @@
  * @copyright Copyright (c) 2021
  * 
  */
+#include <stdio.h>
+#include <stdlib.h>
+
+struct dynamicArray {
+    int size;  // the current number of elements in the array
+    int capacity;  // the maximum number of elements the array can hold
+    int *array;  // the underlying static array
+};
+
+/**
+ * @brief 
+ * 
+ * @return struct dynamicArray 
+ */
+struct dynamicArray newDynamicArray(void) {
+    struct dynamicArray dynArr = {.size=0, .capacity=5, .array=NULL};
+    dynArr.array = (int*) malloc(dynArr.capacity * sizeof(struct dynamicArray));
+
+    return dynArr;
+}
+
+/**
+ * @brief Driver code for minimum priority queue implementation
+ * 
+ * @return int 
+ */
+int main(void) {
+    struct dynamicArray priorityQueue = newDynamicArray();
+
+    return 0;
+}
